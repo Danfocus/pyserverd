beac92f03beebc92d65e8c621dfcc3a2b82ec765
Danfocus@gmail.com <Danfocus@gmail.com@56ff6712-f5cd-11de-93ee-a784c1c6a0aa>
2012-04-27 04:20:22 +0000

    'mysql reconnect' fix

--- src/databases/db_mysql.py
+++ src/databases/db_mysql.py
@@ -8,20 +8,54 @@ from tlv_procs import make_tlv
 
 import MySQLdb #@UnresolvedImport
 
+import logging
+deflogger = logging.getLogger('Logger')
+debuglogger = logging.getLogger('Debug_logger')
+
 class sql(object):
     '''
     classdocs
     '''
+    
+    db_host = None
+    db_port = None
+    db_user = None
+    db_passwd = None
+    db_name = None
+    db_use_unicode = None
+    db_charset = None
+    db = None
 
     def __init__(self, db_host, db_port, db_user, db_passwd, db_name, db_use_unicode, db_charset):
-        db = MySQLdb.connect(host=db_host, port=db_port, user=db_user, passwd=db_passwd, db=db_name, use_unicode=db_use_unicode, charset=db_charset)
-        self.c = db.cursor()
+        self.db_host =  db_host
+        self.db_port = db_port
+        self.db_user = db_user
+        self.db_passwd = db_passwd
+        self.db_name = db_name
+        self.db_use_unicode = db_use_unicode
+        self.db_charset = db_charset
+        self.db_init()
+        
+    def db_init(self):
+        deflogger.info("MySQL connection init", extra={'clientip': '', 'dirn': ''})
+        debuglogger.info("MySQL connection init", extra={'clientip': '', 'dirn': ''})
+        self.db = MySQLdb.connect(host=self.db_host, port=self.db_port, user=self.db_user, passwd=self.db_passwd, db=self.db_name, use_unicode=self.db_use_unicode, charset=self.db_charset)
+        self.c = self.db.cursor()
+        
+    def db_check(self):
+        try:
+            self.db.ping()
+        except:
+            debuglogger.info("MySQL connection broken", extra={'clientip': '', 'dirn': ''})
+            self.db_init()
         
     def db_set_challenge(self, uin, challenge):
+        self.db_check()
         self.c.execute("""REPLACE INTO users_challenges SET users_uin = %s, challenge = %s""", (uin, challenge))
         return self.c.rowcount
     
     def db_get_challenge(self, uin, db_cookie_lifetime):
+        self.db_check()
         self.db_check_challenge_expired(db_cookie_lifetime)
         self.c.execute("""SELECT challenge FROM users_challenges WHERE users_uin = %s LIMIT 1""", (uin))
         challenge = self.c.fetchone()
@@ -33,13 +67,15 @@ class sql(object):
             return None
     
     def db_check_challenge_expired(self, db_cookie_lifetime):
+        self.db_check()
         self.c.execute("""DELETE FROM users_challenges WHERE NOW() > cdate + %s""", db_cookie_lifetime)
                
     def db_set_cookie(self, uin, cookie):
+        self.db_check()
         self.c.execute("""REPLACE INTO users_cookies SET users_uin = %s, cookie = %s""", (uin, cookie))
-        pass
     
     def db_select_users_where(self, sel_, whr_):
+        self.db_check()
         str_ = """SELECT """ + sel_ + """ FROM users WHERE uin = %s LIMIT 1"""
         self.c.execute(str_, (whr_))
         result = self.c.fetchone()
@@ -48,11 +84,13 @@ class sql(object):
         return (None,None)
     
     def db_update_users_where(self, upd_, val_, whr_):
+        self.db_check()
         str_ = """UPDATE users SET """ + upd_ + """ = """ + val_ + """ WHERE uin = %s"""
         self.c.execute(str_, (whr_))
     
     
     def db_get_cookie(self, cookie, db_cookie_lifetime):
+        self.db_check()
         self.db_check_cookie_expired(db_cookie_lifetime)
         self.c.execute("""SELECT users_uin FROM users_cookies WHERE cookie = %s LIMIT 1""", (cookie))
         uin = self.c.fetchone()
@@ -64,12 +102,15 @@ class sql(object):
             return None
     
     def db_check_cookie_expired(self, db_cookie_lifetime):
+        self.db_check()
         self.c.execute("""DELETE FROM users_cookies WHERE NOW() > cdate + %s""", db_cookie_lifetime)       
     
     def db_select_unixtimestamp_users_where(self, sel_, whr_):
+        self.db_check()
         return self.db_select_users_where('UNIX_TIMESTAMP(%s)' % sel_, whr_)
     
     def db_check_ssi(self, uin):
+        self.db_check()
         #ssi_changed = False
         self.c.execute("""SELECT COUNT(*) FROM users_ssi_data WHERE gid = 0 AND id = 0 AND type = 1 AND users_uin = %s""", (uin))
         res = self.c.fetchone()
@@ -106,6 +147,7 @@ class sql(object):
             #ssi_changed = True
             
     def db_get_ssi(self, uin):
+        self.db_check()
         self.c.execute("""SELECT gid, id, type, name, text FROM users_ssi_data WHERE users_uin = %s ORDER BY gid, id, type""", (uin))
         res = self.c.fetchall()
         
